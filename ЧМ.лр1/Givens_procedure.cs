5a877af2a57c36921574a1cdf486697e64a828b5
ПК Домашний <evolext@gmail.com>
2019-09-27 13:31:10 +0700

    final

---
+++ ЧМ.лр1/Givens_procedure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_space
+{
+    class Givens_procedure
+    {
+        public static void Transformation(Matrix A, Matrix Q, Matrix R)
+        {
+            double help1 = .0;
+            double help2 = .0;
+            double cos = .0;
+            double sin = .0;
+
+
+            for (int j = 0; j < A.M - 1; j++)
+            {
+                for (int i = j + 1; i < A.N; i++)
+                {
+                    if (Math.Abs(A.Elem[i][j]) > CONST.Eps)
+                    {
+                        cos = A.Elem[j][j] / Math.Sqrt(Math.Pow(A.Elem[i][j], 2) + Math.Pow(A.Elem[j][j], 2));
+                        sin = A.Elem[i][j] / Math.Sqrt(Math.Pow(A.Elem[i][j], 2) + Math.Pow(A.Elem[j][j], 2));
+
+                        for (int k = j; k < A.M; k++)
+                        {
+                            help1 = cos * A.Elem[j][k] + sin * A.Elem[i][k];
+                            help2 = cos * A.Elem[i][k] - sin * A.Elem[j][k];
+
+                            R.Elem[j][k] = help1;
+                            R.Elem[i][k] = help2;
+                            A.Elem[j][k] = help1;
+                            A.Elem[i][k] = help2;
+                        }
+
+                        for (int k = 0; k < Q.N; k++)
+                        {
+                            help1 = cos * Q.Elem[k][j] + sin * Q.Elem[k][i];
+                            help2 = cos * Q.Elem[k][i] - sin * Q.Elem[k][j];
+
+                            Q.Elem[k][j] = help1;
+                            Q.Elem[k][i] = help2;
+                        }
+
+                    }
+                }
+            }
+
+
+
+            
+
+        }
+    }
+}
---
+++ ЧМ.лр1/GramSchmidt_procedure.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_space
+{
+    // Класс, реализующий процесс ортогонализации
+    // Грама-Шмидта, классический и модернизированный
+    class GramSchmidt_procedure
+    {
+        public static void Classic_method(Matrix A, Matrix Q, Matrix R)
+        {
+            // Вспомогательный вектор
+            Vector Help = new Vector(A.N);
+
+            for (int j = 0; j < A.N; j++)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    for (int k = 0; k < A.N; k++)
+                        R.Elem[i][j] += A.Elem[k][j] * Q.Elem[k][i];
+                }
+
+                for (int k = 0; k < Help.N; k++)
+                    Help.Elem[k] = A.Elem[k][j];
+
+                for (int i = 0; i < j; i++)
+                {
+                    for (int k = 0; k < Help.N; k++)
+                        Help.Elem[k] -= R.Elem[i][j] * Q.Elem[k][i];
+                }
+
+                R.Elem[j][j] = Help.Norma();
+
+                if (R.Elem[j][j] < CONST.Eps)
+                    return;
+
+                for (int k = 0; k < Help.N; k++)
+                    Q.Elem[k][j] = Help.Elem[k] / R.Elem[j][j];
+            }
+
+        }
+
+        public static void Modified_method(Matrix A, Matrix Q, Matrix R)
+        {
+            // Вспомогательный вектор
+            Vector Help = new Vector(A.N);
+
+            for (int j = 0; j < A.N; j++)
+            {
+                for (int k = 0; k < Help.N; k++)
+                    Help.Elem[k] = A.Elem[k][j];
+
+                for (int i = 0; i < j; i++)
+                {
+                    for (int k = 0; k < Help.N; k++)
+                        R.Elem[i][j] += Help.Elem[k] * Q.Elem[k][i];
+
+                    for (int k = 0; k < Help.N; k++)
+                        Help.Elem[k] -= R.Elem[i][j] * Q.Elem[k][i];
+                }
+
+                R.Elem[j][j] = Help.Norma();
+
+                if (R.Elem[j][j] < CONST.Eps)
+                    return;
+
+                for (int k = 0; k < Help.N; k++)
+                    Q.Elem[k][j] = Help.Elem[k] / R.Elem[j][j];
+            }
+
+        }   
+
+    }
+}
