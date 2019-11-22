c6ff4457890efbbfa3be3bc89634b71f3ba1cdf2
evolext <evolext@gmail.com>
2019-11-22 22:39:28 +0700

    ver 1.0

---
+++ Криптография.лр5(графическое)/Криптография.лр5(графическое)/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace encryption
+{
+    public partial class Form1 : Form
+    {
+        // Путь до файла с данными о первой функции
+        public string pathFileFunctionFirst { get; set; }
+        // Путь до файла с данными о второй функции
+        public string pathFileFunctionSecond { get; set; }
+        // Путь до файла с результатом работы программы
+        public string pathFileResult { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Указать путь до файла с данными о первой функции
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Если удалось открыть файл для ввода
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Сохраняем до него путь
+                pathFileFunctionFirst = openFileDialog1.FileName;
+            }
+        }
+
+        // Указать путь до файла с данными о второй функции
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Если удалось открыть файл для ввода
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                // Сохраняем до него путь
+                pathFileFunctionSecond = openFileDialog2.FileName;
+            }
+        }
+
+        // Указать путь до файла сохранения результата
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Сохраняем путь до файла вывода результата
+                pathFileResult = saveFileDialog1.FileName;
+            }
+        }
+
+        // Основной фрагмент
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверка на заполненность всех необходимых данных
+                if (pathFileFunctionFirst == "" || pathFileFunctionFirst == null)
+                    throw new Exception("Укажите файл с данными о функции для первого LFSR-генератора!");
+                if (pathFileFunctionSecond == "" || pathFileFunctionSecond == null)
+                    throw new Exception("Укажите файл с данными о функции для второго LFSR-генератора!");
+                if (textBox1.Text == "" || textBox1.Text == null)
+                    throw new Exception("Укажите начальный элемент для первого LFSR-генератора!");
+                if (textBox2.Text == "" || textBox2.Text == null)
+                    throw new Exception("Укажите начальный элемент для второго LFSR-генератора!");
+                if (textBox3.Text == "" || textBox3.Text == null)
+                    throw new Exception("Укажите размер генерируемой последовательности!");
+                
+
+                // Две функции
+                int size_a = 0; // Размерность первой функции
+                int size_s = 0; // Размерность второй функции
+                List<int> indices_a = PTG.inputFunction(ref size_a, pathFileFunctionFirst);
+                List<int> indices_s = PTG.inputFunction(ref size_s, pathFileFunctionSecond);
+
+                // Стратовое значение x0 для а и x0 для s
+                string startValue_a = textBox1.Text;
+                string startValue_s = textBox2.Text;
+
+                // Проверка на соответствие размеров функции и начального значения x0
+                if (size_a != startValue_a.Length)
+                    throw new Exception("Порядок первой функции и размер первого начального элемента не совпадают!");
+                if (size_s != startValue_s.Length)
+                    throw new Exception("Порядок второй функции и размер второго начального элемента не совпадают!");
+
+                // Проверка на содержание в стартовых значениях недопустимых символов
+                for (int i = 0; i < startValue_a.Length; i++)
+                {
+                    if (startValue_a[i] != '0' && startValue_a[i] != '1')
+                        throw new Exception("Первый начальный элемент содержит недопустимые символы!");
+                }
+                for (int i = 0; i < startValue_s.Length; i++)
+                {
+                    if (startValue_s[i] != '0' && startValue_s[i] != '1')
+                        throw new Exception("Второй начальный элемент содержит недопустимые символы!");
+                }
+
+                // Проверка на нулевые стартовые значения x0
+                bool flag = false;
+                for (int i = 0; i < startValue_a.Length && !flag; i++)
+                {
+                    if (startValue_a[i] == '1')
+                        flag = true;
+                }
+                if (!flag)
+                    throw new Exception("Первый начальный элемент содержит одни нули!");
+                flag = false;
+                for (int i = 0; i < startValue_s.Length && !flag; i++)
+                {
+                    if (startValue_s[i] == '1')
+                        flag = true;
+                }
+                if (!flag)
+                    throw new Exception("Второй начальный элемент содержит одни нули!");
+
+                // Длина сгенерированной последовательности
+                int sizeOfSeries = int.Parse(textBox3.Text);
+                // Получение результата
+                // S - значение статистики для полученной последовательности
+                double Stat = 0;
+                string result = PTG.GenerationPT(startValue_a, startValue_s, indices_a, indices_s, sizeOfSeries, ref Stat);
+                // Вывод реузльтата
+                richTextBox1.Text = result;
+                using (var Sw = new StreamWriter(pathFileResult))
+                {
+                    Sw.WriteLine(result);
+                }
+
+                // Получение и вывод в форму периода полученной последовательности
+                // (если его возможно определеить аналитически)
+                textBox4.Text = (PTG.PeriodOfPTG(startValue_a, startValue_s, indices_a, indices_s)).ToString();
+                // Значение статистики
+                textBox5.Text = Stat.ToString();
+                // Вывод значения критической статистики для 2-1 степени свободы и a = 0,05
+                textBox6.Text = "3,84";
+                // Принятие или отвержение гипотезы о виде распределения
+                if (Stat > 3.84)
+                {
+                    textBox7.BackColor = Color.Red;
+                    textBox7.Text = "Отвергается";
+                }
+                else
+                {
+                    textBox7.BackColor = Color.LightGreen;
+                    textBox7.Text = "Подтверждается";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Криптография.лр5(графическое)/Криптография.лр5(графическое)/PTG.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace encryption
+{
+    class PTG
+    {
+        // Ввод информации о полиноме из файла
+        public static List<int> inputFunction(ref int size, string PATH)
+        {
+            // size - максимальная степень полинома, она же - размерность регистра
+            // Индексы ненулевых коэффициентов массива
+            List<int> indicesOfCoef = new List<int>();
+            // Вспомогательная переменная
+            int help = 0;
+            string buf = System.String.Empty;
+
+            using (var Sr = new StreamReader(PATH))
+            {
+                while ((buf = Sr.ReadLine()) != null)
+                {
+                    // Проверка на недопустимые символы
+                    if (buf != "0" && buf != "1")
+                        throw new Exception("Файл с данными о функции содержит недопустимые символы!");
+                    help = int.Parse(buf);
+                    // Сохраняем индексы ненулевых коэффициентов полинома
+                    if (help != 0 && size != 0)
+                        indicesOfCoef.Add(size - 1);
+                    size++;
+                }
+                size--;
+            }
+
+            return indicesOfCoef;
+        }
+
+        // Функция генерации, задается стартовое значение и размер
+        // сгенерированной последовательности
+        public static void GenerationLFSR(string startValue, int power, List<int> indices)
+        {
+            // Создание регистра с начальным заданным значением
+            int[] register = new int[startValue.Length];
+            for (int i = 0; i < register.Length; i++)
+                register[i] = startValue[i] - '0';
+
+            // Основной цикл
+            for (int k = 0; k < power; k++)
+            {
+                for (int i = 0; i < register.Length; i++)
+                    Console.Write(register[i]);
+                // Выводим значение последнего бита регистра
+                Console.Write("   " + register[register.Length - 1] + "\n");
+                // Вызов одной итерации LFSR
+                OneIterationLFSR(ref register, indices);
+            }
+        }
+
+        // Одна итерация LFSR
+        public static int OneIterationLFSR(ref int[] register, List<int> indices)
+        {
+            int sum = 0;
+            // Считаем новое значение 0-бита регистра
+            for (int i = 0; i < indices.Count; i++)
+                sum += register[indices[i]];
+            //sum += register[register.Length - 1 - indices[i]];
+            // Выполняем сдвиг вправо на один бит
+            for (int i = register.Length - 1; i > 0; i--)
+                register[i] = register[i - 1];
+            // Устанавливаем новое значение первого бита
+            register[0] = sum % 2;
+            return register[register.Length - 1];
+        }
+
+        // Псевдослучайная генерация с прореживанием
+        // Задается стартовое значение x0,
+        // Длина сгенерированной последовательности power
+        // индексы коэффициентов для элементарной и селектирующей последовательности
+        public static string GenerationPT(string startValue_a, string startValue_s, List<int> indices_a, List<int> indices_s, int power, ref double Stat)
+        {
+            // Сгенерированная последовательность
+            string result = System.String.Empty;
+
+            // Счетчики единиц и нулей для оценивания распределения
+            int zero_counter = 0, unit_counter = 0;
+            // n - размер системы
+            int n = power;
+
+            // Создаем регистры для элементарной и селектирующей последовательности
+            int[] register_a = new int[startValue_a.Length];
+            int[] register_s = new int[startValue_s.Length];
+            for (int i = 0; i < register_a.Length; i++)
+                register_a[i] = startValue_a[i] - '0';
+
+            for (int i = 0; i < register_s.Length; i++)
+                register_s[i] = startValue_s[i] - '0';
+
+            int a = register_a[register_a.Length - 1];
+            int s = register_s[register_s.Length - 1];
+            if (s == 1)
+            {
+                // Вывод последнего бита
+                result += a.ToString();
+                power--;
+                if (a == 1)
+                    unit_counter++;
+                else
+                    zero_counter++;
+            }
+
+            while (power > 0)
+            {
+                a = OneIterationLFSR(ref register_a, indices_a);
+                s = OneIterationLFSR(ref register_s, indices_s);
+
+                if (s == 1)
+                {
+                    // Вывод последнего бита
+                    result += a.ToString();
+                    power--;
+                    if (a == 1)
+                        unit_counter++;
+                    else
+                        zero_counter++;
+                }
+            }
+
+            // Оцениваем вид распределения по критерию хи-квадрат
+            Stat = n * 2 * (Math.Pow((float)unit_counter / n - (float)1 / 2, 2) + Math.Pow((float)zero_counter / n - (float)1 / 2, 2));
+            //Console.WriteLine("S = {0}; t = {1}", S, 3.84);
+
+
+
+            return result;
+        }
+
+        // Функция получения значения периода для заданной
+        // комбинации исходных данных
+        public static int PeriodOfSeries(string startValue, List<int> indices)
+        {
+            // По свойству, период не превышает данное число
+            int limit = (int)Math.Pow(2, startValue.Length);
+            // Период последовательности
+            int T = 1;
+
+            // Создаем два регистра
+            int[] register_original = new int[startValue.Length];
+            int[] register_side = new int[startValue.Length];
+            for (int i = 0; i < register_original.Length; i++)
+            {
+                register_original[i] = startValue[i] - '0';
+                register_side[i] = register_original[i];
+            }
+
+            for (int k = 0; k < limit; k++, T++)
+            {
+                OneIterationLFSR(ref register_side, indices);
+
+                if (RegisterCompare(register_original, register_side))
+                    return T;
+            }
+
+            // Ошибка
+            return -1;
+        }
+
+        // Функция сравнения двух регистров, необходимо
+        // для получение периода LFSR
+        public static bool RegisterCompare(int[] register_original, int[] register_side)
+        {
+            // Переменные должны совпадать по размеру
+            if (register_original.Length != register_side.Length)
+                return false;
+
+            for (int i = 0; i < register_original.Length; i++)
+            {
+                if (register_original[i] != register_side[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        // Нахождение НОД x и y
+        public static int gcd(int x, int y)
+        {
+            if (y == 0)
+                return x;
+            else
+                return gcd(y, x % y);
+        }
+
+        // Проверка чисел на взаимную простоту
+        public static bool MutualSimplicity(int x, int y)
+        {
+            return ((gcd(x, y) == 1) ? true : false);
+        }
+
+        // Нахождение периода генератора псевдослучаных числе с прореживанием
+        public static int PeriodOfPTG(string startValue_a, string startValue_s, List<int> indices_a, List<int> indices_s)
+        {
+            // Период элементарной последовательности
+            int period_a = PeriodOfSeries(startValue_a, indices_a);
+            // Период селектирующей последовательности
+            int period_s = PeriodOfSeries(startValue_s, indices_s);
+
+            // Если взаимно простые числа, то можем найти период
+            if (MutualSimplicity(period_a, period_s))
+                return (int)((Math.Pow(2, startValue_a.Length) -1) * Math.Pow(2, startValue_s.Length - 1));
+            else
+                // Не являются взаимно простыми, период можно найти только вручную
+                return -1;
+        }
+    }
+}
