6f6f7078e05d9653cc5a1db9211031dd61d75f42
evolext <evolext@gmail.com>
2020-03-20 12:46:53 +0700

    v2.0

--- web.lr1(docker)/index.js
+++ web.lr1(docker)/index.js
@@ -1,13 +1,11 @@
-// Заупск БД:
-// "C:\Program Files\MongoDB\Server\4.2\bin\mongod.exe" --dbpath="c:\data\db"
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+const fs = require("fs");
 
 // Модуль mongodb
-//var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 // Путь до БД
 var path = "mongodb://localhost:27017/";
@@ -23,19 +21,30 @@ app.get('/', function(request, response) {
 
 io.sockets.on('connection', function (socket) {
 
-    // Вывод комментариев из БД
-    // MongoClient.connect(path, function (err, client) {
-    //     var db = client.db('database');
+    // Тут должен быть вывод комментариев из БД, но пока даже не получается писоединиться
+    MongoClient.connect(path, function (err, client) {
+        console.log('Connected to MongoDB server');
+
+        var db = client.db('config');
+        // var db = client.db('comments');
+
+        // db.collection('comments').find({}).toArray(function (err, result) {
+        //    if (err) throw err;
   
-    //     db.collection('comments').find({}).toArray(function (err, result) {
-    //        if (err) throw err;
+        //    for (let i = 0; i < result.length; i++)
+        //       socket.emit('set_mess', { "name": result[i]["name"], "comment": result[i]["comment"] });
   
-    //        for (let i = 0; i < result.length; i++)
-    //           socket.emit('set_mess', { "name": result[i]["name"], "comment": result[i]["comment"] });
-  
-    //     });
-    //     client.close();
-    //  });
+        // });
+        client.close();
+    });
+
+    // Вывод комментариев из БД (вариант с текстовым файлом)
+    // var data = fs.readFileSync("info/test.txt", "utf8", function() {});
+    // var split_lines = data.split('\n');
+    // for (let i = 0; i < split_lines.length - 1; i++)
+    // {
+    //     socket.emit('set_mess', { "name": split_lines[i].split('\t')[0], "comment": split_lines[i].split('\t')[1]});
+    // }
 
 
     // Сервер получает сообщение от клиента
@@ -48,6 +57,10 @@ io.sockets.on('connection', function (socket) {
         //     collection.insertOne(data);
         //     client.close();
         //  });
+
+        // Добавление комментария в базу данных (варриант с текстовым файлом)
+        //fs.appendFile("info/test.txt", data.name + '\t' + data.comment + '\n', function() {});
+
 
         // Отображение комментария на странице
         io.sockets.emit('set_mess', data);
