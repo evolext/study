488a1c87834dd39777318f23a9cff8d74f54cddb
evolext <evolext@gmail.com>
2020-04-15 10:25:54 +0700

    script rewrite

--- web_lr_2/mongo-init.js
+++ web_lr_2/mongo-init.js
@@ -1,21 +1,59 @@
-version: "3.1"
-secrets:
-  mongo-admin-passwd:
-    file: mongo-admin-passwd
-services:
-  mongodb:
-    # Базовый образ
-    image: mongo:3
-    # Имя создаваемого контейнера
-    container_name: db
-    # Переменные окружения
-    environment:
-      MONGO_INITDB_ROOT_USERNAME: admin
-      MONGO_INITDB_ROOT_PASSWORD_FILE: /run/secrets/mongo-admin-passwd
-    # Используемые порты
-    ports:
-      - 27017:27017
-    volumes:
-      - ./mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js
-    secrets:
-      - mongo-admin-passwd
+// Данные о пользователях БД
+var users = {
+  // Общее число пользователей
+  count: 5,
+  allUsers: [
+    {
+      'user': 'cerym',
+      'pwd': '123',
+      'roles': [
+        { 'role': 'dbAdmin', 'db': 'database' }
+      ]
+    },
+    {
+      'user': 'enavi',
+      'pwd': '123',
+      'roles': [
+        { 'role': 'userAdmin', 'db': 'database' }
+      ]
+    },
+    {
+      'user': 'kreer',
+      'pwd': '123',
+      'roles': [
+        { 'role': 'dbOwner', 'db': 'database' }
+      ]
+    },
+    {
+      'user': 'pabrg',
+      'pwd': '123',
+      'roles': [
+        { 'role': 'read', 'db': 'database' }
+      ]
+    },
+    {
+      'user': 'lelki',
+      'pwd': '123',
+      'roles': [
+        { 'role': 'readWrite', 'db': 'database' }
+      ]
+    }
+  ]
+}
+
+// Создаем несистемную БД для необходиимых действий
+db = db.getSiblingDB('database');
+
+// Создаем три коллекции
+db.createCollection('Dates');
+db.createCollection('Units');
+db.createCollection('Users');
+
+// Создаем нужных пользователей БД
+for (let i = 0; i < users.count; i++)
+  db.createUser(users.allUsers[i]);
+
+// Инициализируем по одному документу в каждой коллекции
+db.Dates.insertOne({ "Unit ID": "0", "Time": "00:00", "Value": "0" });
+db.Units.insertOne({ "Name": "admin", "Secret": "admin", "Descr": "0" });
+db.Users.insertOne({ "Name": "admin", "User ID": "0", "Descr": "0", "Secret": "admin" });
